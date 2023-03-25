456cf3d2010e6fc4b13942723cbffa38c75aeeb5
username <vasanthkumars2025@gmail.com>
2023-03-25 08:47:13 +0530

    added basic files to build the project

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+import { Navbar, Homepage, Exchanges, Cryptocurrencies, CryptoDetails, News } from './components';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='app'>
+      <div className='navbar'>
+        <Navbar />
+      </div>
+      <div className='main'>
+        <Layout>
+          <div className='routes'>
+            <Routes>
+              <Route exact path='/'>
+                <Homepage />
+              </Route>
+              <Route exact path='/exchanges'>
+                <Exchanges />
+              </Route>
+              <Route exact path='/cryptocurrencies'>
+                <Cryptocurrencies />
+              </Route>
+              <Route exact path='/crypto/:coinId'>
+                <CryptoDetails />
+              </Route>
+              <Route exact path='/news'>
+                <News />
+              </Route>
+            </Routes>
+          </div>
+        </Layout>
+      </div>
+      <div className='footer'>
+
+      </div>
+    </div>
+  )
+};
+
+export default App;
