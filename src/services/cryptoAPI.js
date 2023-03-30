cb06bae285f1fd9e864bb04e9674e3877ee8c79c
username <vasanthkumars2025@gmail.com>
2023-03-30 09:23:33 +0530

    added details for crypto

--- src/services/cryptoAPI.js
+++ src/services/cryptoAPI.js
@@ -14,9 +14,17 @@ export const cryptoApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
     getCryptos: builder.query({
-      query: (count) => createRequest('/coins?limit=' + count),
+      query: (count) => createRequest('/coins?limit=' + count)
+    }),
+
+    getCryptoDetails: builder.query({
+      query: (coinId) => createRequest(`/coin/${coinId}`)
+    }),
+
+    getCryptoHistory: builder.query({
+      query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history?timeperiod=${timeperiod}`),
     }),
   }),
 });
 
-export const { useGetCryptosQuery } = cryptoApi;
+export const { useGetCryptosQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery } = cryptoApi;
